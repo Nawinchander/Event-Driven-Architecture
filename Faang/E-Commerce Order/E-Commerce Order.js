57da8373c6958dd8e4ba9cd96ec75526cc3129c6
Nawin chander <nawinnapzy289@gmail.com>
2026-04-21 11:53:15 +0530

    add

--- Faang/E-Commerce Order/E-Commerce Order.js
+++ Faang/E-Commerce Order/E-Commerce Order.js
@@ -1,2 +1,46 @@
 //// E-Commerce Order Pipeline (Amazon-like)
 
+/// Kafka-style abstraction
+
+
+class EventBus {
+  constructor() {
+    this.handlers = {};
+  }
+
+  subscribe(event, handler) {
+    if (!this.handlers[event]) this.handlers[event] = [];
+    this.handlers[event].push(handler);
+  }
+
+  publish(event, payload) {
+    if (this.handlers[event]) {
+      this.handlers[event].forEach(handler => handler(payload));
+    }
+  }
+}
+
+const bus = new EventBus();
+
+// Consumers
+bus.subscribe("OrderPlaced", (order) => {
+  console.log("Payment processed for", order.id);
+});
+
+bus.subscribe("OrderPlaced", (order) => {
+  console.log("Inventory reserved for", order.id);
+});
+
+bus.subscribe("OrderPlaced", (order) => {
+  console.log("Shipping initiated for", order.id);
+});
+
+// Producer
+function placeOrder(order) {
+  console.log("Order placed:", order.id);
+  bus.publish("OrderPlaced", order);
+}
+
+placeOrder({ id: "A1001" });
+
+
