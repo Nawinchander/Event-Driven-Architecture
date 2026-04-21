f9d9ff80973f1d9a1b6444c0671152d9593a27b7
Nawin chander <nawinnapzy289@gmail.com>
2026-04-21 11:50:58 +0530

    add

--- Simple Example.js
+++ Simple Example.js
@@ -2,3 +2,29 @@
 
 // Using Node.js EventEmitter:
 
+const EventEmitter = require("events");
+
+class Bus extends EventEmitter {}
+const bus = new Bus();
+
+// Consumers
+bus.on("OrderCreated", (order) => {
+  console.log("Processing payment for", order.id);
+});
+
+bus.on("OrderCreated", (order) => {
+  console.log("Reserving inventory for", order.id);
+});
+
+bus.on("OrderCreated", (order) => {
+  console.log("Sending email for", order.id);
+});
+
+// Producer
+function createOrder(order) {
+  console.log("Order created:", order.id);
+  bus.emit("OrderCreated", order);
+}
+
+createOrder({ id: "ORD101" });
+
