524753e1d700df3f079ab63652ac9c0cacafcdef
Nawin chander <nawinnapzy289@gmail.com>
2026-04-21 11:55:01 +0530

    add

--- Faang/Ride Booking Workflow/Ride Booking Workflow.js
+++ Faang/Ride Booking Workflow/Ride Booking Workflow.js
@@ -3,3 +3,24 @@
 
 /// Ride Service → Kafka → Driver/ETA/Pricing services
 
+const bus = new EventBus();
+
+bus.subscribe("RideRequested", (ride) => {
+  console.log("Finding driver for", ride.userId);
+});
+
+bus.subscribe("RideRequested", (ride) => {
+  console.log("Calculating ETA for", ride.userId);
+});
+
+bus.subscribe("RideRequested", (ride) => {
+  console.log("Calculating surge price for", ride.userId);
+});
+
+function requestRide(ride) {
+  console.log("Ride requested");
+  bus.publish("RideRequested", ride);
+}
+
+requestRide({ userId: "USER1" });
+
